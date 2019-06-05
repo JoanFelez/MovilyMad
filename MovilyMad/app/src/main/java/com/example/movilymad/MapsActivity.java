6342c9514203b35a37df282e6511073c8b12f811
Unknown <mercabarna87@gmail.com>
2019-06-05 22:41:17 +0200

    Add car positions

--- MovilyMad/app/src/main/java/com/example/movilymad/MapsActivity.java
+++ MovilyMad/app/src/main/java/com/example/movilymad/MapsActivity.java
@@ -19,8 +19,11 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
@@ -28,6 +31,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     GoogleApiClient mGoogleApiClient;
     Location mLastLocation;
     LocationRequest mLocationRequest;
+    ArrayList<LatLng> zityMarkers;
+    ArrayList<LatLng> biciMadMarkers;
+    ArrayList<LatLng> eMovMarkers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +58,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         } else {
             mMap.setMyLocationEnabled(true);
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+            setMarkers();
+            for(LatLng location : zityMarkers)
+                mMap.addMarker(new MarkerOptions().position(location).title("Zity").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+            for(LatLng location : biciMadMarkers)
+                mMap.addMarker(new MarkerOptions().position(location).title("BiciMad").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+            for(LatLng location : eMovMarkers)
+                mMap.addMarker(new MarkerOptions().position(location).title("Emov").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
 
         }
     }
@@ -69,9 +83,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onLocationChanged(Location location) {
         mLastLocation = location;
         LatLng latlng = new LatLng(40.46407, -3.69276);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-        mMap.addMarker(new MarkerOptions().position(latlng).title("Me"));
+        mMap.addMarker(new MarkerOptions().position(latlng).title("Me").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
     }
 
     @Override
@@ -96,5 +108,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    private void setMarkers(){
+        zityMarkers = new ArrayList<>();
+        biciMadMarkers = new ArrayList<>();
+        eMovMarkers = new ArrayList<>();
+
+        zityMarkers.add(new LatLng(40.46759298106364, -3.693600498355295));
+        zityMarkers.add(new LatLng(40.467429736651184, -3.697012268222238));
+        zityMarkers.add(new LatLng(40.46279342968996, -3.6999948846467987));
+
+        biciMadMarkers.add(new LatLng(40.464295366988466, -3.6900599824556366));
+        biciMadMarkers.add(new LatLng(40.45756903840472, -3.68297895065632));
+        biciMadMarkers.add(new LatLng(40.46103024305326,-3.695746265567209));
+
+        eMovMarkers.add(new LatLng(40.463111259517575,-3.698122501373291));
+        eMovMarkers.add(new LatLng(40.46951055485471,-3.690762519836426));
+        eMovMarkers.add(new LatLng(40.46569064068097,-3.680140972137451));
     }
 }
