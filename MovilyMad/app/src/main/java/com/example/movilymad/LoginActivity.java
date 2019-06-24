ecfedf707fc67000b5a865ebc8dca83b1c571c19
Joan Felez <joan.felez@movicoders.com>
2019-06-24 15:02:26 +0200

    Add hide/show button for pwd

--- MovilyMad/app/src/main/java/com/example/movilymad/LoginActivity.java
+++ MovilyMad/app/src/main/java/com/example/movilymad/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.example.movilymad;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,10 +21,11 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText mEmail, mPass;
-    private Button mLogin, mRegistration;
+    private Button mLogin, mRegistration, showHideBtn;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +36,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getInstance().getCurrentUser();
-                if (user != null){
+                if (user != null) {
                     Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
                     startActivity(intent);
                     finish();
@@ -47,6 +50,7 @@ public class LoginActivity extends AppCompatActivity {
 
         mLogin = findViewById(R.id.login);
         mRegistration = findViewById(R.id.registration);
+        showHideBtn = findViewById(R.id.showHideBtn);
 
         mRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,9 +60,9 @@ public class LoginActivity extends AppCompatActivity {
                 mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(!task.isSuccessful())
+                        if (!task.isSuccessful())
                             Toast.makeText(LoginActivity.this, "Sign up error", Toast.LENGTH_SHORT).show();
-                        else{
+                        else {
                             String user_id = mAuth.getCurrentUser().getUid();
                             DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("users").child(user_id);
                             current_user_db.setValue(true);
@@ -77,9 +81,9 @@ public class LoginActivity extends AppCompatActivity {
                 mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(!task.isSuccessful())
+                        if (!task.isSuccessful())
                             Toast.makeText(LoginActivity.this, "Login error", Toast.LENGTH_SHORT).show();
-                        else{
+                        else {
                             String user_id = mAuth.getCurrentUser().getUid();
                             DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("users").child(user_id);
                             current_user_db.setValue(true);
@@ -88,6 +92,21 @@ public class LoginActivity extends AppCompatActivity {
                 });
             }
         });
+
+        showHideBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (showHideBtn.getText().toString().equals("Show")) {
+                    mPass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    showHideBtn.setText("Hide");
+                } else {
+                    mPass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    showHideBtn.setText("Show");
+                }
+            }
+
+        });
+
     }
 
     @Override
